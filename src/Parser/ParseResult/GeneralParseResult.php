27a7e223a6bd4c0f29c1931112e755dec94dc8d7
Yuriy Serdyuk <yury@serdyuk.pp.ua>
2019-12-01 21:15:54 +0200

    IParseResultList and IParseResult no more implement IParseResultItemContainer

---
+++ src/Parser/ParseResult/GeneralParseResult.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace PhoenyxStudio\Parser\ParseResult;
+
+use PhoenyxStudio\Parser\ParseResultItemContainer\GeneralParseResultItemContainer;
+use PhoenyxStudio\Parser\ParseResultList\IParseResultList;
+
+class GeneralParseResult extends GeneralParseResultItemContainer implements IParseResult
+{
+    protected $lists;
+    public function addList(IParseResultList $list) : int
+    {
+        $newElementKey = 0;
+        $keys = array_keys($this->lists);
+        if (!empty($keys)) {
+            $latestElementKey = $keys[count($keys) - 1];
+            $newElementKey = $latestElementKey + 1;
+        }
+        /**
+         * for PHP >=7.3 we will use the following way instead of code above
+         * $newElementKey = array_key_last($this->lists) + 1;
+         */
+        $this->items[$newElementKey] = $list;
+        return $newElementKey;
+    }
+}
---
+++ src/Parser/ParseResultList/GeneralParseResultList.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace PhoenyxStudio\Parser\ParseResultList;
+
+use PhoenyxStudio\Parser\ParseResultItemContainer\GeneralParseResultItemContainer;
+
+class GeneralParseResultList extends GeneralParseResultItemContainer implements IParseResultList
+{
+
+}
